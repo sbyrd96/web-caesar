04e0f414e4ebd6c622e8be95459e0fbffb14a4b3
Susan Byrd <byrd96@outlook.com>
2019-05-09 23:02:45 -0500

    added to git ignore

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>        
     <body>
@@ -31,7 +31,7 @@ form = """
                 <input type="text" name="rot" value="0">
                 <p class="error"></p>
             </div>
-            <textarea type="text" name="text"></textarea>
+            <textarea type="text" name="text">{0}</textarea>
             <br>
             <input type="submit">
         </form>
@@ -41,7 +41,7 @@ form = """
 
 @app.route("/")
 def index():
-  return form
+  return form.format("")
 
 
 @app.route("/webcaesar", methods=["POST"])
@@ -49,6 +49,6 @@ def encrypt():
     rotationamt = int(request.form["rot"])
     phrase = request.form["text"]
     encrypted = rotate_string(phrase, rotationamt)
-    return '<h1>' + encrypted + '</h1>'
+    return form.format(encrypted)
 
 app.run()
